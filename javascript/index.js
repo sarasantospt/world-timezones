74ea40c185dd84b438b759f865a9f4a66ff3bd6b
sarasantospt <sarasantos95pt@gmail.com>
2023-08-29 22:04:37 +0100

    Added emojis next to city names

--- javascript/index.js
+++ javascript/index.js
@@ -33,7 +33,7 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
